31f8285a93a39596f069fe3b1e25f9dbf9931040
Anthony Foster <foster_a86@yahoo.com>
2013-02-25 16:44:07 -0600

    Added twitter functionality.

---
+++ iceBerg/src/com/example/iceberg/TwitGet.java
@@ -0,0 +1,60 @@
+package com.example.iceberg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class TwitGet extends AsyncTask<String,Void,String>{
+	
+	protected String doInBackground(String... urls) {
+		URI uri = null;
+		try {
+			uri = new URI(urls[0]);
+		} catch (URISyntaxException e1) {
+		}
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(uri);
+		HttpResponse response;
+		try {
+		  response = httpclient.execute(httpget);
+		  HttpEntity entity = response.getEntity();
+		  StringBuilder str = new StringBuilder();
+		  String line = null;
+		  if (entity != null) {
+		    InputStream instream = entity.getContent();
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(instream));		
+			while((line = reader.readLine()) != null)
+			{
+			    str.append(line);
+			}
+		    instream.close();
+		    
+		  }
+		  return str.toString();
+		} catch (ClientProtocolException e) {
+	
+		} catch (IOException e){
+		  Log.e("aferror", "ERROR: Failed to open GET_REQUEST "+ e);
+		}
+		return null;
+	}
+
+	protected void OnPostExecute(String result){
+		
+	}
+	
+}
--- iceBerg/src/com/example/iceberg/Twitter.java
+++ iceBerg/src/com/example/iceberg/Twitter.java
@@ -3,33 +3,25 @@ package com.example.iceberg;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
 
-
-
-
-
 public class Twitter extends ListFragment {
 //stores player twitter handles	
-    String[] twitterHandle = new String[] {
-            "Luke Johnson",
-            "Justin Wood",
-            "Dominik Shine",
-            "Mike Mckee",
-    };
+    String[] twitterHandle = new String[15];
 //stores actual tweets
-    String[] tweets = new String[]{
-            "had a huge fight last night #go stars",
-            "first goal ever! #starsFlyTogether",
-            "goalie fight #winning",
-            "crowd was insane last night #stars",
-        };
+    String[] tweets = new String[15];
     	
 	public Twitter() {
 	}
@@ -37,7 +29,42 @@ public class Twitter extends ListFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		// Each row in the list stores country name, currency and flag
+		//Call twitter api to get tweets
+		//For some reason having rpp=12 means we get 15 results.
+		String result = null;
+		try {
+			result = new TwitGet().execute("http://search.twitter.com/search.json?q=celticstalk&rpp=12&include_entities=true&with_twitter_user_id=true&result_type=mixed").get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.i("af",result);
+		JSONArray jArray = null;
+		try {
+			JSONObject jObject = new JSONObject(result);
+			jArray = jObject.getJSONArray("results");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.i("af",""+jArray.length());
+		for(int i=0; i < jArray.length(); i++){
+			JSONObject oneObject;
+			try {
+				oneObject = jArray.getJSONObject(i);
+				twitterHandle[i] = oneObject.getString("from_user");
+				tweets[i] = oneObject.getString("text");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	    
+		}
+		
+		
+		// Each row in the list stores twitter handle and tweet
         List<HashMap<String,String>> dataList = new ArrayList<HashMap<String,String>>();
  
         for(int i=0;i< twitterHandle.length;i++){
@@ -56,8 +83,7 @@ public class Twitter extends ListFragment {
         // Ids of views in listview_layout
         int[] to = {R.id.twitterHandle,R.id.tweets};
         SimpleAdapter adapter = new SimpleAdapter(getActivity(), dataList, R.layout.twitter_listview_layout, from, to);
-        setListAdapter(adapter);
-        //new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_2,mListItems));  
+        setListAdapter(adapter);  
 
         return view;
 	}
